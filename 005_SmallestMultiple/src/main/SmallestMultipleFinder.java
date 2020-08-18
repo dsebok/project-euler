1779b5d3b5dec04bdfbbd43d141d7db9f18eff36
David Sebok <david_sebok@epam.com>
2020-08-18 21:50:27 +0200

    Create parameterized solution in SmallestMultipleFinder for exercise 005

--- 005_SmallestMultiple/src/main/SmallestMultipleFinder.java
+++ 005_SmallestMultiple/src/main/SmallestMultipleFinder.java
@@ -1,9 +1,43 @@
 package main;
 
+import java.util.ArrayList;
+
 public class SmallestMultipleFinder {
 
+    public long smallestMultipleOf(int max) {
+        ArrayList<Long> factors = createFactorList(max);
+        return multiply(factors);
+    }
+
+    private Long multiply(ArrayList<Long> factors) {
+        return factors.stream().reduce(1l, (a, b) -> a*b);
+    }
+
+    private ArrayList<Long> createFactorList(int max) {
+        ArrayList<Long> factors = new ArrayList<>();
+        for (long i = 2l; i <= max; i++) {
+            long reduced = reduce(i, factors);
+            if (reduced > 1) {
+                factors.add(reduced);
+            }
+        }
+        return factors;
+    }
+
+    private long reduce(long current, ArrayList<Long> factors) {
+        return factors.stream()
+                .reduce(current, (a, b) -> {
+                    if (a % b == 0) {
+                        return a / b;
+                    } else {
+                        return a;
+                    }
+                });
+    }
+
     public static void main(String[] args) {
-        System.out.println(2*3*2*5*7*2*3*11*13*2*17*19);
+        SmallestMultipleFinder finder = new SmallestMultipleFinder();
+        System.out.println(finder.smallestMultipleOf(20));
     }
 
 }
