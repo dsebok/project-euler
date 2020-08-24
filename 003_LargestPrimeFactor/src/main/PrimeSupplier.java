32a761559eb0a4b105c29c9ab0f58056e0644c8d
David Sebok <david_sebok@epam.com>
2020-08-25 00:44:28 +0200

    Optimize PrimeSupplier

--- 003_LargestPrimeFactor/src/main/PrimeSupplier.java
+++ 003_LargestPrimeFactor/src/main/PrimeSupplier.java
@@ -1,44 +1,67 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.LongSupplier;
 
 public class PrimeSupplier implements LongSupplier {
-//2,3,5,7,11 caches for instant skips
-    private long current = 1;
-    private final ArrayList<Long> primes = new ArrayList<>();
+
+    private int index = 0;
+    private final long[] counters = {1, 1, 1, 1, 1};
+    private final ArrayList<Long> primes = new ArrayList(List.of(2L,3L,5L,7L,11L));
 
     @Override
     public long getAsLong() {
-        long result = getNextPrime();
-        save(result);
-        return result;
+        primes.add(getNextPrime());
+        return primes.get(index++);
     }
 
     private long getNextPrime() {
-        long number = ++current;
+        long number = getNextNonTrivialNumber();
         while (!isPrime(number)) {
-            number++;
+            number = getNextNonTrivialNumber();
         }
         return number;
     }
 
-    private void save(long number) {
-        current = number;
-        primes.add(number);
+    private long getNextNonTrivialNumber() {
+        long number = primes.get(index);
+        boolean isTrivial = true;
+        while (isTrivial) {
+            ++number;
+            increaseCacheCounters();
+            isTrivial = Arrays.stream(counters).anyMatch(counter -> counter == 0);
+        }
+        return number;
+    }
+
+    private void increaseCacheCounters() {
+        if (++counters[0] == 2) {
+            counters[0] = 0;
+        }
+        if (++counters[1] == 3) {
+            counters[1] = 0;
+        }
+        if (++counters[2] == 5) {
+            counters[2] = 0;
+        }
+        if (++counters[3] == 7) {
+            counters[3] = 0;
+        }
+        if (++counters[4] == 11) {
+            counters[4] = 0;
+        }
     }
 
     private boolean isPrime(long number) {
-        if (number == 2) {
-            return true;
-        }
         return primes.stream()
                 .filter(prime -> prime <= Math.sqrt(number))
                 .noneMatch(prime -> number % prime == 0);
     }
 
     public long getCurrent() {
-        return current;
+        return primes.get(index);
     }
 
     public ArrayList<Long> getPrimes() {
--- 003_LargestPrimeFactor/src/test/PrimeSupplierTest.java
+++ 003_LargestPrimeFactor/src/test/PrimeSupplierTest.java
@@ -23,12 +23,22 @@ public class PrimeSupplierTest {
         long actual3 = underTest.getAsLong();
         long actual4 = underTest.getAsLong();
         long actual5 = underTest.getAsLong();
+        long actual6 = underTest.getAsLong();
+        long actual7 = underTest.getAsLong();
+        long actual8 = underTest.getAsLong();
+        long actual9 = underTest.getAsLong();
+        long actual10 = underTest.getAsLong();
         //THEN
         assertEquals(2, actual1);
         assertEquals(3, actual2);
         assertEquals(5, actual3);
         assertEquals(7, actual4);
         assertEquals(11, actual5);
+        assertEquals(13, actual6);
+        assertEquals(17, actual7);
+        assertEquals(19, actual8);
+        assertEquals(23, actual9);
+        assertEquals(29, actual10);
     }
 
 }
